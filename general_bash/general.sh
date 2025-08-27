7f0047ba2fae7a3334c09a635fb7634add465d4d
Arman Moussavi <ArmanMoussavi2027@u.northwestern.edu>
2025-08-26 19:42:52 -0500

    Create general.sh

---
+++ general_bash/general.sh
@@ -0,0 +1,12 @@
+
+# Create a conda environment
+module load python/anaconda3.6
+conda create --name env_name
+source activate env_name
+
+
+# Create a compressed archive of a directory using tar and gzip
+tar -czvf archive_name.tar.gz /path/to/your_directory/
+
+# List size of all directories in current directory
+du -hd1
