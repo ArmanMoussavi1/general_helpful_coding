7b8c7419d17348b0730f6afc1e8593497f8a699c
Arman Moussavi <ArmanMoussavi2027@u.northwestern.edu>
2025-09-04 20:18:44 -0500

    Update build_stable_lammps_automated.sh

--- build_lammps_HPC_cluster/build_stable_lammps_automated.sh
+++ build_lammps_HPC_cluster/build_stable_lammps_automated.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Written by Arman Moussavi
-# Last updated: 04-03-2025
+# Last updated: 09-04-2025
 
 # Clone the stable branch of the LAMMPS repository into a directory named "lammps_stable"
 git clone -b stable https://github.com/lammps/lammps.git lammps_stable
@@ -15,12 +15,15 @@ mkdir build && cd build/
 module purge
 
 # Load required modules for compiling LAMMPS (make require newer versions of cmake)
-module load cmake/3.15.4 mpi/openmpi-4.1.1-gcc.10.2.0 gcc/11.2.0 hdf5/1.10.8-openmpi-3.1.3-gcc-8.4.0 
+module load gcc/10.2.0 mpi/openmpi-4.1.1-gcc.10.2.0 cmake/3.22.4
 
 # Run CMake to configure the LAMMPS build  
     # Edit package options below as needed to include/exclude specific features
     # Remove unnecessary packages to optimize compilation time and reduce memory usage
-cmake -D PKG_DPD-BASIC=yes \
+cmake -D CMAKE_C_COMPILER=$(which mpicc) \
+      -D CMAKE_CXX_COMPILER=$(which mpicxx) \
+      -D CMAKE_CXX_STANDARD=17 \
+      -D PKG_DPD-BASIC=yes \
       -D PKG_EXTRA-FIX=yes \
       -D PKG_GRANULAR=yes \
       -D PKG_MOLECULE=yes \
@@ -35,12 +38,6 @@ cmake -D PKG_DPD-BASIC=yes \
       -D PKG_USER-MISC=yes \
       -D DOWNLOAD_EIGEN3=yes \
       -D PKG_MANYBODY=yes \
-      -D PKG_INTEL=yes
-      -D PKG_USER-COLVARS=yes \
-      -DCMAKE_C_COMPILER=gcc \
-      -DCMAKE_CXX_COMPILER=g++ \
-      -DCMAKE_Fortran_COMPILER=gfortran \
-      -D FFT=MKL
       ../cmake  # Specify the source directory for CMake configuration
 
 # Compile and install LAMMPS using 4 parallel threads
@@ -48,7 +45,8 @@ make -j4 install
 
 
 # NOTE: To run LAMMPS after installation, ensure that the correct modules are loaded:
-# module load gcc/10.4.0-gcc-4.8.5 hdf5/1.10.7-openmpi-intel-2021.4.0 git/2.37.2 intel-oneapi-mkl/2023.1.0-intel-2021.4.0 mpi/openmpi-4.1.6rc2-intel-2021.4.0 cmake/3.26.3-intel-2021.4.0 intel-oneapi-compilers/2021.4.0-gcc-10.4.0 ffmpeg/4.2.2-intel-19.0.5.281 fftw/3.3.4-openmpi-4.0.5-intel-19.0.5.281
+# module load gcc/10.2.0 mpi/openmpi-4.1.1-gcc.10.2.0 cmake/3.22.4
+
 
 
 # Example command to run LAMMPS with 4 MPI processes:
